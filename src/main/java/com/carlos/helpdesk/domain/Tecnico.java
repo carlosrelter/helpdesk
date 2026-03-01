7a4f9c38c3f16604cfe4d10c924a3bb7dc7e84ad
carlosrelter <reltercontato@gmail.com>
2026-03-01 16:20:51 -0300

    create tecnico novo

--- src/main/java/com/carlos/helpdesk/domain/Tecnico.java
+++ src/main/java/com/carlos/helpdesk/domain/Tecnico.java
@@ -2,8 +2,10 @@ package com.carlos.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.carlos.helpdesk.domian.enums.Perfil;
+import com.carlos.helpdesk.dtos.TecnicoDTO;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
@@ -25,6 +27,17 @@ public class Tecnico extends Pessoa {
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Tecnico(TecnicoDTO tecnicoDTO) {
+		super();
+		this.id = tecnicoDTO.getId();
+		this.nome = tecnicoDTO.getNome();
+		this.cpf = tecnicoDTO.getCpf();
+		this.email = tecnicoDTO.getEmail();
+		this.senha = tecnicoDTO.getSenha();
+		this.perfis = tecnicoDTO.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = tecnicoDTO.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/carlos/helpdesk/services/TecnicoService.java
+++ src/main/java/com/carlos/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carlos.helpdesk.domain.Tecnico;
+import com.carlos.helpdesk.dtos.TecnicoDTO;
 import com.carlos.helpdesk.repository.TecnicoRepository;
 import com.carlos.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 	
 	public List<Tecnico> findAll(){
 		return tecnicoRepository.findAll();
+	}
+	
+	public Tecnico create(TecnicoDTO tecnicoDTO) {
+		tecnicoDTO.setId(null);
+		Tecnico tecnico = new Tecnico(tecnicoDTO);
+		return tecnicoRepository.save(tecnico);
 	}
 }
