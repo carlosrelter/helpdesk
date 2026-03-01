d200ff317a90d6420c8b02ce3dbfabdb199f22b6
carlosrelter <reltercontato@gmail.com>
2026-03-01 15:56:15 -0300

    busca todos os tecnicos

--- src/main/java/com/carlos/helpdesk/services/TecnicoService.java
+++ src/main/java/com/carlos/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.carlos.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
 		return tecnico.orElseThrow(() -> new ObjectNotFoundException("Tecnico não encontrado! id: " + id));
+	}
+	
+	public List<Tecnico> findAll(){
+		return tecnicoRepository.findAll();
 	}
 }
