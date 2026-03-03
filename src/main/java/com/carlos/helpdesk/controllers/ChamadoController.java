3fea00f72535971d493fe52dcda4c48f87eecb7e
carlosrelter <reltercontato@gmail.com>
2026-03-03 08:04:35 -0300

    getMapping do find by id e criação do find all

--- src/main/java/com/carlos/helpdesk/controllers/ChamadoController.java
+++ src/main/java/com/carlos/helpdesk/controllers/ChamadoController.java
@@ -1,7 +1,11 @@
 package com.carlos.helpdesk.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +21,16 @@ public class ChamadoController {
 	@Autowired
 	private ChamadoService chamadoService;
 	
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
 		Chamado chamado = chamadoService.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(chamado));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll(){
+		List<Chamado> list = chamadoService.findAll();
+		List<ChamadoDTO> listDTO = list.stream().map(chamado -> new ChamadoDTO(chamado)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/carlos/helpdesk/services/ChamadoService.java
+++ src/main/java/com/carlos/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.carlos.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class ChamadoService {
 	public Chamado findById(Integer id) {
 		Optional<Chamado> chamado = chamadoRepository.findById(id);
 		return chamado.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado! ID: " + id));
+	}
+	
+	public List<Chamado> findAll(){
+		return chamadoRepository.findAll();
 	}
 }
