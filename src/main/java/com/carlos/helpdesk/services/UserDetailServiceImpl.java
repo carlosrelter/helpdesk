8740a0a21ba47dcf5e35b9591e055ff52b0aa27d
carlosrelter <reltercontato@gmail.com>
2026-03-15 16:40:37 -0300

    implementação do use service

---
+++ src/main/java/com/carlos/helpdesk/services/UserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.carlos.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.carlos.helpdesk.domain.Pessoa;
+import com.carlos.helpdesk.repository.PessoaRepository;
+import com.carlos.helpdesk.security.UserSS;
+
+public class UserDetailServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Pessoa> user  = pessoaRepository.findByEmail(email);
+		if(user.isPresent()) {
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getSenha(), user.get().getPerfis());
+		}
+		throw new UsernameNotFoundException(email);
+	}
+
+}
